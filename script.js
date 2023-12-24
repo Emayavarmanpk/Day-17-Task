e90906c4f6247e516d37f963de8143b5205f3739
EmayavarmanPK <148607935+Emayavarmanpk@users.noreply.github.com>
2023-12-25 01:18:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
-const div = document.createElement("div");
-const div1 = document.createElement("div");
-const cardHeader = document.createElement("div");
-
-
-div.setAttribute("class", "container");
-div1.setAttribute("class", "row");
-fetch("https://restcountries.com/v3.1/all").then((data) => data.json()).then((ele) => {
-    for (i = 0; i < ele.length; i++) {
-        const res = document.createElement("div")
-        res.setAttribute("class", "col-sm-6 col-md-4 col-lg-4 col-xl-4 g-4")
-        res.innerHTML = `<div class="card h-100">
-        <div class="card-header">
-        <h1 class="text-center" id="title">${ele[i].name.common} </h1>
-        </div>
-        <img src="${ele[i].flags.png}" class="card-img-top" alt="flag">
-        <div class="card-body id="cbody">
-        <div class="card-text">
-        <p>  Region : ${ele[i].region} <br>
-        Population : ${ele[i].population}</p>
-        <button type="button" class="btn btn-primary" onclick="getData('${ele[i].name.common}')">Click for Weather</button>
-        </div>
-       </div>
-        `
-        div1.append(res);
-    }
-})
-function getData(name) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=4c0033e593e7af096d7e7ec870266f97`).then((res) => res.json()).then((data) => {
-        var countryname = data.name;
-        if (countryname === name) {
-            alert(`Weather is ${data.name} is 
-           Minimum ${data.main.temp_min}
-           Maxmum ${data.main.temp_max}
-           Wind speed ${data.wind.speed}`
-            )
-        }
-        else{
-            alert("This country wether not show in the api")
-        }
-    })
+function element(country, classname, id, text){
+  let crtag = document.createElement(country);
+  crtag.classList = classname;
+  crtag.id =id;
+  crtag.innerHTML = text;
+  return crtag;
 }
 
-div.append(div1);
-document.body.append(div);
+
+let container = element("div", "container","", "");
+const h1 = element("h1", "text-center", "title", "Rest Countries Weather From API");
+const row = element("div", "row", "", "");
+
+const response = fetch("https://restcountries.com/v3.1/all");
+response
+.then((data)=> data.json())
+.then((card1)=>{
+  for(let i=0;i <card1.length;i++){
+     const col = document.createElement("div");
+     col.classList ="col-sm-6 col-md-4 col-lg-4 col-xl-4";
+col.innerHTML =`
+<div class= "card h-100">
+<div class="card-header">
+<h5 class = "card-title text-center">${card1[i].name.common}</h5>
+</div>
+<div class = "img-box">
+<img src="${card1[i].flags.png}" class="card-img-top" alt="..."/>
+</div>
+<div class = "card-body">
+<div class="card-text text-center">Region: ${card1[i].region}</div>
+<div class="card-text text-center">Capital: ${card1[i].capital}</div>
+<div class="card-text text-center">Countrycode: ${card1[i].cca3}</div>
+<div class="card-text text-center">Population: ${card1[i].population}</div>
+
+<button class = "btn btn-primary" Onclick = "getWeatherData('${card1[i].name.common}',${i})"> Click Here for Weather</button>
+<p class ="weatherInfo-${i}"></p>
+<div class="some-class"></div>
+</div>
+</div>`;
+row.append(col);
+  }
+  let btns = document.querySelectorAll("button");
+  btns.forEach((btn, index)=>{
+    btn.addEventListener("click",()=>{
+      let latlng = card1[index].latlng;
+      let lat = latlng[0];
+      let lng = latlng[1];
+                                                
+      let weatherAPI = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=429a23dee47c5972025ec631391cc139&units=metric`);
+         weatherAPI
+         .then((data)=> data.json())
+         .then((value)=>         
+alert( `Weather of ${card1[index].name.common}= ${Math.floor(value.main.temp)}°C`)         
+         );
+
+    });
+  });
+
+});
+document.body.append(h1,container);
+container.append(row);
